a7258f88b8b7f9d70a5126a3c930b830aeefafba
Leo4815162342 <leonid.pyrlya@gmail.com>
2018-06-03 14:08:59 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+
+
+
+    <div v-if="newsLoading">LOADING</div>
+    <div v-else>FINISED LOADING</div>
+    <div v-for="(lang, code) in langs">
+      <div @click="fetchNews(code)">{{lang}}, {{code}}</div>
+    </div>
+    <div class="list" v-if="news.length">
+      
+      <div class="list__item" v-for="{ EventName, CurrencyCode, ReleaseDate, Id } in news" :key="Id">
+        <strong>{{EventName}}</strong>, <strong>{{CurrencyCode}}</strong>, <span>{{ReleaseDate}}</span>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+
+import proxiedFetch from './proxiedFetch';
+
+// http://xkcd.com/info.0.json
+// https://quotes.tickmill.com/livegraph_new/cache/small.php?cors=tm
+const API_BASE_URL = 'https://www.mql5.com/ru/economic-calendar/widget/content';
+
+const NEWS_API_URL = 'https://www.mql5.com/ru/economic-calendar/widget/content?date_mode=2&from=2018-05-04T00%3A00%3A00&to=2018-05-06T23%3A59%3A59';
+
+const new_test = 'https://www.mql5.com/ru/economic-calendar/widget/content?date_mode=2&from=2017-04-29T00%3A00%3A00to=2017-04-30T00%3A00%3A00';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
+  },
+  data() {
+    return  {
+      langs: {
+        ru: 'Русский',
+        en: 'English',
+        es: 'Español',
+        zh: '中文',
+        pt: 'Português',
+        ja: '日本語',
+        de: 'Deutsch'
+      },
+      news: [],
+      newsLoading: true
+    }
+  },
+  methods: {
+    async fetchNews(lang = 'en', dateFrom = '2017-04-20', dateTo = '2017-04-21') {
+      
+      this.$emit('startFetching');
+
+      const url = `https://www.mql5.com/${lang}/economic-calendar/widget/content?date_mode=2&from=${dateFrom}T00:00:00&to=${dateTo}T23:59:59`;
+
+      const res = await proxiedFetch(encodeURI(url));
+      const textData = await res.text();
+      const jsonData = JSON.parse(textData.replace(/<!--[\s\S]*?-->/g, ""));
+
+      this.news = jsonData;
+
+      this.$emit('finishFetching');
+
+    },
+    bindHandlers() {
+
+      this.$on('startFetching',  _ => this.newsLoading = true);
+      this.$on('finishFetching',  _ => this.newsLoading = false);
+
+    }
+  },
+  created: async function() {
+
+    this.bindHandlers();
+    // this.fetchNews('ja');
+
+    // setTimeout(() => {
+    //   this.fetchNews('de');
+    // }, 5000)
+
   }
 }
 </script>
 
 <style>
+
+body {
+  margin: 0;
+  padding: 0;
+  background: #dfdfdf;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Work Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="header">
+      <div class="header__inner">
+        <div class="header__logo">
+          <img src="./../assets/newspaper.svg" width="24" height="24">
+          <h1>Economic News</h1>
+        </div>
+        <div class="header__menu">
+          <div class="header__menu-item">Jun 5 - Jun 7</div>
+          <div class="header__menu-item">English</div>
+          <div class="header__menu-item">Updated: 12.12.2018 12:12</div>
+        </div>
+        
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    // msg: String
+  },
+  created() {
+    
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  height: 50px;
+  background-color: #2f2f2ffa;
+}
+
+.header__inner {
+  display: flex;
+  justify-content: space-between;
+  max-width: 960px;
+  margin: 0 auto;
+  height: inherit;
+}
+
+.header__logo {
+  display: flex;
+  align-items: center;
+  color: #fff;
+}
+
+.header__logo h1 {
+  margin: 0;
+  font-size: 20px;
+  font-weight: 400;
+  color: #fff;
+  margin-left: 5px;
+}
+
+.header__menu {
+  color: #a8a8a8;
+  height: inherit;
+}
+
+.header__menu-item {
+  display: inline-block;
+  margin: 0 10px;
+  height: inherit;
+  line-height: 50px;
+}
+
+.header__menu-item:last-child {
+  margin-right: 0;
+}
+
+.header__menu-item:hover {
+  color: #fff;
+}
+</style>
