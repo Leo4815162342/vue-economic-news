e452a4b05e431c2ddb81e6029e2f4b965a2061aa
Leo4815162342 <leonid.pyrlya@gmail.com>
2018-08-19 21:38:50 +0200

    localized chart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,5 +1,6 @@
 <template>
   <GChart
+    :settings="{'packages':['corechart'],'language': currentLang}"
     type="LineChart"
     :data="formattedChartData"
     :options="chartOptions"
@@ -8,7 +9,11 @@
 
 <script>
 
+
+import { mapState } from 'vuex';
 import { GChart } from 'vue-google-charts';
+
+import translations from './../translations.js';
 
 export default {
   name: 'Chart',
@@ -23,22 +28,24 @@ export default {
       legend: 'none',
       focusTarget: 'category',
       chartArea:{width: '100%', top: 20, left: 40, right: 25, bottom: 50 },
-      hAxis: {
-        format: 'y'
-      }
     }
   }),
   computed: {
+    ...mapState([
+      'currentLang'
+    ]),
+    chartLabels() {
+      return ['time', 'actual', 'forecast'].map(item => {
+        return translations[item][this.currentLang];
+      })
+    },
     formattedChartData() {
-      return [['Time', 'Actual', 'Forecast'],...this.raw.map(item => {
+      return [this.chartLabels,...this.raw.map(item => {
         const { dateTs, actualVal, forecastVal} = item;
         return [new Date(dateTs), actualVal, forecastVal];
       }).sort((a,b) => a[0] - b[0])];
     }
-  },
-  // methods: {
-
-  // }
+  }
 }
 </script>
 
--- src/components/NewsTableHeader.vue
+++ src/components/NewsTableHeader.vue
@@ -25,76 +25,13 @@
 
 
 import { mapState } from 'vuex';
+import translations from './../translations.js';
 
 export default {
   name: 'NewsTableHeader',
   data() {
     return {
-      translations: {
-        time: {
-          en: 'Time',
-          ru: 'Время',
-          zh: '时间',
-          es: 'Hora',
-          pt : 'Hora',
-          ja: '時刻',
-          de: 'Zeit'
-        },
-        currency: {
-          en: 'Currency',
-          ru: 'Валюта',
-          zh: '货币',
-          es: 'Divisa',
-          pt : 'Moeda',
-          ja: '通貨',
-          de: 'Währung'
-        },
-        event: {
-          en: 'Event',
-          ru: 'Событие',
-          zh: '事件',
-          es: 'Evento',
-          pt : 'Evento',
-          ja: 'イベント',
-          de: 'Ereignis'
-        },
-        actual: {
-          en: 'Actual',
-          ru: 'Актуальное',
-          zh: '实际值',
-          es: 'Actual',
-          pt : 'Atual',
-          ja: '実際',
-          de: 'Aktuell'
-        },
-        forecast: {
-          en: 'Forecast',
-          ru: 'Прогноз',
-          zh: '预测值',
-          es: 'Pronóstico',
-          pt : 'Projeção',
-          ja: '予測',
-          de: 'Prognose'
-        },
-        previous: {
-          en: 'Previous',
-          ru: 'Предыдущее',
-          zh: '以前',
-          es: 'Anterior',
-          pt : 'Prévio',
-          ja: '前',
-          de: 'Vorherige'
-        },
-        importance: {
-          en: 'Importance',
-          ru: 'Важность',
-          zh: '重要性',
-          es: 'Importancia',
-          pt : 'Importância',
-          ja: '重要性',
-          de: 'Bedeutung'
-        }
-      }
+      translations
     }
   },
   computed: {
