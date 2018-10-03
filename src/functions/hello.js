749e78897b69a003e73af2b8e5e7a4bc914bbf23
Leo4815162342 <leonid.pyrlya@gmail.com>
2018-10-03 22:32:56 +0200

    added dev proxy for netlify lambda

--- src/functions/hello.js
+++ src/functions/hello.js
@@ -1,6 +1,9 @@
-exports.handler = function(event, context, callback) {
-    callback(null, {
-    statusCode: 200,
-    body: "Hello, World222"
-    });
+exports.handler = async function(event, context) {
+
+    console.log(event);
+
+    return {
+        statusCode: 200,
+        body: "Hello, World333"
+    };
 }
